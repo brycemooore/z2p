8619150e358a887edb9826c767ff7d531e267785
brycemooore <brycemooore@gmail.com>
2023-09-20 15:32:38 -0400

    post subscriptions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,16 @@
+use sqlx::{Executor, PgConnection, Connection,  PgPool};
+use uuid::Uuid;
+use zero2prod::configuration::{DatabaseSettings, get_configuration};
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool
+}
 #[tokio::test]
 async fn health_check_works() {
     //Arrange
-    let address = format!("{}/health_check", spawn_app());
+    let app = spawn_app().await;
+    let address = format!("{}/health_check", app.address);
     //bring in reqwest
     let client = reqwest::Client::new();
 
@@ -16,10 +25,96 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-fn spawn_app() -> String {
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_data() {
+    //Arrange
+    let app = spawn_app().await;
+    let address = format!("{}/subscriptions", app.address);
+
+    let client = reqwest::Client::new();
+    let body = "name=bryce%20moore&email=brycemooore%40gmail.com";
+
+    //Act
+    let response = client
+        .post(&address)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to send post request to /subscriptions");
+
+    let saved = sqlx::query!("SELECT email, name from subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    //Assert
+    assert_eq!(200, response.status().as_u16());
+    assert_eq!("bryce moore", saved.name);
+    assert_eq!("brycemooore@gmail.com", saved.email);
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_data() {
+    //Arrange
+    let app = spawn_app().await;
+    let address = format!("{}/subscriptions", app.address);
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=bryce%20moore", "no email"),
+        ("email=brycemooore%40gmail.com", "no name"),
+        ("", "no name or email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        //Act
+        let response = client
+            .post(&address)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to send post request to /subscriptions");
+
+        //Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "API did not fail with 400 bad request when payload had {}.",
+            error_message
+        )
+    }
+}
+async fn spawn_app() -> TestApp {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+    let mut configuration = get_configuration().expect("Failed to read configuration");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
+    let server = zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    TestApp{address, db_pool: connection_pool}
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    //create database
+    let mut connection = PgConnection::connect(
+        &config.connection_string_without_db()
+    )
+        .await
+        .expect("Failed to connect to database");
+    connection.execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to database");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate database");
+
+    connection_pool
 }
